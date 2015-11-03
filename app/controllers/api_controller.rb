505ed44d0d251550497190b300fefb8b4aff224e
EJLarcs <ejlarcombe@gmail.com>
2015-11-03 17:26:14 +0000

    basic API controller and playing with image downloads

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,39 @@
+require 'httparty'
+require 'open-uri'
 class ApiController < ApplicationController
+
+  def download
+    logger.debug(request.content_type)
+    logger.debug(request.format)
+    logger.debug(params[:url])
+    save_image(params[:url])
+    render json: { msg: "ok"}
+  end
+
+  def save_image(url)
+    response = HTTParty.get(url)
+    logger.debug(response)
+  end
+
+  def save_image(url)
+   response = HTTParty.get(url)
+   url_array = url.split("")
+   i = nil
+   url_array.each_with_index{|v, k| i = k if v == "/"}
+   filename = url[i..-1]
+   if response.success?
+     File.open("#{filename}", "wb") do |f|
+       f.write response.parsed_response
+     end
+   else
+     raise response.response
+   end
+  end
+
 end
+
+
+string = "this is a string"
+string.slice!(2)        #=> "i"
+string.slice!(3..6)     #=> " is "
+string.slice!(/s.*t/)   #=> "sa st"
